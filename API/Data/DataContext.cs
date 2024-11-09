638ccd23f0263e864aa33ae6c2aac422d3c20c5f
mhelwa <mhelwa@2p.com.sa>
2024-11-09 21:41:53 +0300

    start Dating App

---
+++ API/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data;
+
+public class DataContext(DbContextOptions options) : DbContext(options)  //to give it options we need to create constructor
+{
+    public DbSet<AppUser> Users {get; set;}
+}
