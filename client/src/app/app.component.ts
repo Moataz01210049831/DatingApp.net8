e720a4345975bfd10731f5f371f4d1650ce673d6
mhelwa <mhelwa@2p.com.sa>
2024-11-13 21:38:51 +0300

    update: angular app

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit,OnDestroy{
+  title = 'Dating App';
+  users:any;
+ private http=inject(HttpClient);
+ ngOnInit(): void {
+   this.http.get('http://localhost:5233/api/Users').subscribe({
+  next:(data)=>{this.users=data},
+  error:()=>{},
+  complete:()=>{console.log(this.users)}
+  
+  })
+ }
+
+ ngOnDestroy(): void {
+   
+ }
+  constructor(private Httpclient:HttpClient){
+
+  }
+}
